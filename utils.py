e6fd48de26462d9dd3f51f8b8cae5b4a98886cb6
Paul Lee <paul.lee@ximplar.com>
2024-06-17 14:40:44 +0800

    Added __main__ to utils.py

--- utils.py
+++ utils.py
@@ -6,14 +6,19 @@ def cleanup_assistants():
     client = openai.OpenAI()
 
     # List all assistants
-    assistants = client.beta.assistants.list()
+    assistants = client.beta.assistants.list(limit=100)
     for assistant in assistants.data:
-        if "alpha_" in assistant.name or "cathbot_" in assistant.name:
-            print(f"XXX DELETING: Assistant ID: {assistant.id}, Name: {assistant.name}")
-            client.beta.assistants.delete(assistant.id)
-            assistants.data.remove(assistant)
+        if "demo" in assistant.name or "cathbot_" in assistant.name:
+            try:            
+                print(f"XXX DELETING: Assistant ID: {assistant.id}, Name: {assistant.name}")
+                client.beta.assistants.delete(assistant.id)
+                assistants.data.remove(assistant)
+            except:
+                print(f"XXX FAILED TO DELETE: Assistant ID: {assistant.id}, Name: {assistant.name}")
 
     print("+++ REMAINING ASSISTANTS +++")
     for assistant in assistants.data:
         print(f"Assistant ID: {assistant.id}, Name: {assistant.name}")
 
+if __name__ == "__main__":
+    cleanup_assistants()
